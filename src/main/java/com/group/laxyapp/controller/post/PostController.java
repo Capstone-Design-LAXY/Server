abeacbef3f8fe44e21a46ea16ed8be28bb44557e
hYe807n <rlagkdus55@gmail.com>
2024-08-13 21:59:09 +0900

    fix: "/post" state 401 예외 처리 후 state 상태 오류 수정

--- src/main/java/com/group/laxyapp/controller/post/PostController.java
+++ src/main/java/com/group/laxyapp/controller/post/PostController.java
@@ -7,7 +7,10 @@ import com.group.laxyapp.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -22,11 +25,11 @@ public class PostController {
     public ResponseEntity<String> uploadPost(@RequestBody PostUploadRequest uploadRequest) {
         try {
             postService.uploadPost(uploadRequest);
+            return ResponseEntity.ok(PostState.SUCCESS_UPLOAD.getMessage());
         } catch (IllegalArgumentException exception) {
             return ResponseEntity.badRequest().body(exception.getMessage());
         }
-
-        return ResponseEntity.ok(PostState.SUCCESS_UPLOAD.getMessage());
     }
+
 
 }
