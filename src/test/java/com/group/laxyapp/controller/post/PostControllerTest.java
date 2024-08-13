e3f8f8cc4db16bde60924130df099028f3e2f0bc
hYe807n <rlagkdus55@gmail.com>
2024-08-13 19:26:53 +0900

    test: 게시글 내용이 공백이라면 예외 발생

---
+++ src/test/java/com/group/laxyapp/controller/post/PostControllerTest.java
@@ -0,0 +1,22 @@
+package com.group.laxyapp.controller.post;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.group.laxyapp.domain.enums.PostState;
+import com.group.laxyapp.domain.post.Post;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PostControllerTest {
+
+    @DisplayName("게시글 내용이 공백이라면 예외 처리")
+    @Test
+    public void isNullContents() {
+        try {
+            new Post("title", "", "tag", "photofile");
+        } catch (IllegalArgumentException exception) {
+            Assertions.assertEquals(exception.getMessage(), PostState.NULL_CONTENT.getMessage());
+        }
+    }
+}
